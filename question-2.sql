78b636894ea07db98214719cdb5084173177cd38
Arushi Agarwal <121698199+arushiarushi@users.noreply.github.com>
2023-04-11 18:48:50 -0700

    Add files via upload

--- question-2.sql
+++ question-2.sql
@@ -1,1 +1,6 @@
 -- Show the sales agent's full name and invoices associated with each sales agent.
+SELECT e.FirstName || ' ' || e.LastName AS SalesAgentName, i.InvoiceId
+FROM employees AS e
+JOIN customers AS c ON e.EmployeeId = c.SupportRepId
+JOIN invoices AS i ON c.CustomerId = i.CustomerId
+ORDER BY e.FirstName, e.LastName;
