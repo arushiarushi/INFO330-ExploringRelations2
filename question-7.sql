cb4e84092aa48c18ef4dafb8cf4779ad10b33f3a
Arushi Agarwal <121698199+arushiarushi@users.noreply.github.com>
2023-04-11 22:23:06 -0700

    Add files via upload

--- question-7.sql
+++ question-7.sql
@@ -1,1 +1,5 @@
 -- Find tracks (id, name, and composer) that have never been purchased (that is, they aren't part of a line item in an invoice).
+SELECT t.TrackId as TrackId, t.Name as TrackName, t.Composer as TrackComposer
+FROM tracks as t
+LEFT JOIN invoice_items AS i ON t.TrackId =  i.TrackId
+WHERE i.InvoiceLineId is NULL;
