e990dfe29c92726475908fd1e7dd485999ce03b9
Arushi Agarwal <121698199+arushiarushi@users.noreply.github.com>
2023-04-11 22:42:58 -0700

    Update question-4.sql

--- question-4.sql
+++ question-4.sql
@@ -3,6 +3,6 @@ SELECT e.FirstName || ' ' ||e.LastName AS SalesAgentName,  SUM(i.Total) AS Total
 FROM employees as e
 JOIN customers AS c ON e.EmployeeId = c.SupportRepId
 JOIN invoices AS i ON c.CustomerId = i.CustomerId
-WHERE i.InvoiceDate = '2010'
+WHERE i.InvoiceDate BETWEEN '2010-01-01 00:00:00' AND '2010-12-31 00:00:00'  
 GROUP BY SalesAgentName
 ORDER BY TotalSales DESC;
