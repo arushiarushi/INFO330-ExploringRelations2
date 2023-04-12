0b12fdb39235216e7a7c5bdce9f76db8b814b856
Arushi Agarwal <121698199+arushiarushi@users.noreply.github.com>
2023-04-11 22:36:35 -0700

    Update question-1.sql

--- question-1.sql
+++ question-1.sql
@@ -1,6 +1,6 @@
 -- Show the customer's full name, Invoice ID, Date of the invoice and billing country of customers who are from Brazil.
 SELECT c.FirstName || ' ' || c.LastName AS FullName,
 i.InvoiceID, i.InvoiceData, i.BillingCountry
-FROM Customer c
-JOIN Invoice i ON c.CustomerId = i.CustomerId
+FROM customers c
+JOIN invoices i ON c.CustomerId = i.CustomerId
 WHERE c.Country = 'Brazil'; 
