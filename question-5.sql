75c920e442936ccc2489ad934d9b4adf3bee94d2
Arushi Agarwal <121698199+arushiarushi@users.noreply.github.com>
2023-04-11 22:45:09 -0700

    Update question-5.sql

--- question-5.sql
+++ question-5.sql
@@ -1,6 +1,6 @@
 -- What was the most purchased track of 2013?
 SELECT t.Name as MostPurchasedTrack, SUM(it.Quantity) AS TotalTrackPurchases 
 FROM tracks AS t, invoice_items AS it, invoices AS i
-WHERE it.TrackId = t.trackId AND it.InvoiceId = i.InvoiceId AND i.InvoiceDate BETWEEN '2013-01-01' AND '2013-12-31'
+WHERE it.TrackId = t.TrackId AND it.InvoiceId = i.InvoiceId AND i.InvoiceDate BETWEEN '2013-01-01' AND '2013-12-31'
 GROUP BY t.name
 ORDER BY TotalTrackPurchases DESC
