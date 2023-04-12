9f6e68bae334527a9ab619eafcf891c5d134c832
Arushi Agarwal <121698199+arushiarushi@users.noreply.github.com>
2023-04-11 19:00:13 -0700

    Add files via upload

--- question-3.sql
+++ question-3.sql
@@ -1,1 +1,7 @@
 -- Provide a query that includes the purchased track name AND artist name with each invoice line item.
+SELECT i.InvoiceLineId, t.Name AS PurchasedTrackName, ar.Name AS Artist_Name
+FROM invoice_items AS i
+JOIN tracks AS t ON i.TrackId = t.TrackId
+JOIN albums AS al ON t.AlbumId = al.AlbumId
+JOIN artists AS ar ON al.ArtistId = ar.ArtistId
+ORDER BY i.InvoiceLineId
