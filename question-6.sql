7402c100e2d358cc55fb470fcf490b3e2da7f305
Arushi Agarwal <121698199+arushiarushi@users.noreply.github.com>
2023-04-11 22:04:49 -0700

    Add files via upload

--- question-6.sql
+++ question-6.sql
@@ -1,1 +1,6 @@
 -- Find tracks (id, name and composer) that are part of a line in an invoice.
+SELECT t.TrackId as TrackId, t.name AS TrackName, t.Composer AS TrackComposer
+FROM tracks AS t, invoice_items AS it
+WHERE t.TrackId = it.TrackId
+GROUP BY t.TrackId
+ORDER BY t.TrackId ASC; 
